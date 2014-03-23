37c911d84e6b8a04cf87cd762f317756aa2eae32
Evan Sosenko <razorx@evansosenko.com>
2014-03-23 15:54:48 -0700

    Moved core classes to scipy_data_fitting.figure.

--- plots/fit.py
+++ plots/fit.py
@@ -1,147 +1,25 @@
-import json
-import numpy
+from scipy_data_fitting.figure import Fit
 
-class Fit:
-    """
-    Loads a fit from a json file into a `Fit` object.
-    """
+class Fit(Fit):
 
     def __init__(self, path=None):
-        self.path = path
-        """
-        Path to the file containing fit to load.
-        """
-
-    @property
-    def path(self):
-        return self._path
-
-    @path.setter
-    def path(self, value):
-        self._path = value
-
-    @property
-    def data(self):
-        """
-        Data as a [`numpy.ndarray`][1] in the form
-
-            #!python
-            [
-                [ x1, x2, x3, ... ],
-                [ y1, y2, y3, ...]
-            ]
-
-        [1]: http://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.html
-        """
-        if not hasattr(self, '_data'): self._load()
-        return self._data
-
-    @data.setter
-    def data(self, value):
-        self._data = value
-
-    @property
-    def fit(self):
-        """
-        Fit points as a [`numpy.ndarray`][1] in the form
-
-            #!python
-            [
-                [ x1, x2, x3, ... ],
-                [ y1, y2, y3, ...]
-            ]
-
-        [1]: http://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.html
-        """
-        if not hasattr(self, '_fit'): self._load()
-        return self._fit
-
-    @fit.setter
-    def fit(self, value):
-        self._fit = value
-
-    @property
-    def meta(self):
-        """
-        A dictionary of metadata related to the fit.
-        """
-        if not hasattr(self, '_meta'): self._load()
-        return self._meta
-
-    @meta.setter
-    def meta(self, value):
-        self._meta = value
-
-    @property
-    def maps(self):
-        """
-        A dictionary of A dictionaries.
-        Each dictionary defines a map which is used to extend the metadata.
-        """
-        if not hasattr(self, '_maps'):
-            maps = {}
-            maps['siunitx'] = {
-                'T': r'\tesla',
-                'μm': r'\micro \meter',
-                'Ω': r'\ohm',
-                'mΩ': r'\milli \ohm',
-                'mS': r'\milli \siemens',
-                'Ω nm': r'\ohm \nano \meter',
-                'nm':  r'\nano \meter',
-                'kΩ': r'\kilo \ohm',
-                'ps': r'\pico \second',
-                'm^2 / s': r'\square \meter \per \second',
-            }
-            maps['tex_symbol'] = {
-                'τ': r'\tau',
-                'Ω_C': 'R_C',
-            }
-            maps['value_transforms'] = {
-                '__default__': lambda x: round(x, 2),
-            }
-            self._maps = maps
-        return self._maps
-
-    @maps.setter
-    def maps(self, value):
-        self._maps = value
-
-    def _load(self):
-        raw = json.load(open(self.path))
-        self.data = numpy.array(raw['data']).T
-        self.fit = numpy.array(raw['fit']).T
-        self.meta = raw['meta']
-        for key in self.meta: self._extend_meta(self.meta[key])
-
-    def _extend_meta(self, meta):
-        if isinstance(meta, str): return None
-        if isinstance(meta, dict): meta = [meta]
-        for param in meta:
-            if 'name' in param: key = param['name']
-            if 'symbol' in param: key = param['symbol']
-
-            if 'units' in param:
-                param['siunitx'] = self._get_siunitx(param['units'])
-            else:
-                param['siunitx'] = ''
-
-            param['tex_symbol'] = self._get_tex_symbol(key)
-
-            if 'value' in param:
-                param['disply_value'] = str(self._value_transform(key)(param['value']))
-
-    def _get_siunitx(self, key):
-        table = self.maps['siunitx']
-        if key in table: return table[key]
-        return ''
-
-    def _get_tex_symbol(self, key):
-        table = self.maps['tex_symbol']
-        if key in table: return table[key]
-        return key
-
-    def _value_transform(self, key):
-        if key in self.maps['value_transforms']:
-            return self.maps['value_transforms'][key]
-        else:
-            return self.maps['value_transforms']['__default__']
+        super().__init__(path=path)
+        self.maps['siunitx'] = {
+            'T': r'\tesla',
+            'μm': r'\micro \meter',
+            'Ω': r'\ohm',
+            'mΩ': r'\milli \ohm',
+            'mS': r'\milli \siemens',
+            'Ω nm': r'\ohm \nano \meter',
+            'nm':  r'\nano \meter',
+            'kΩ': r'\kilo \ohm',
+            'ps': r'\pico \second',
+            'm^2 / s': r'\square \meter \per \second',
+        }
+        self.maps['tex_symbol'] = {
+            'τ': r'\tau',
+            'Ω_C': 'R_C',
+        }
+        self.maps['value_transforms'] = {
+            '__default__': lambda x: round(x, 2),
+        }
--- plots/plot_difference.py
+++ plots/plot_difference.py
@@ -6,29 +6,17 @@ from plot import Plot
 
 class Plot(Plot):
 
-    def add_xlabel(self):
-        x = self.fit.meta['independent']
-        self.subplot.set_xlabel(
-            '$' + x['tex_symbol'] + r'$ $(\si{' + x['siunitx'] +  r'})$')
-
     def add_ylabel(self):
         y = self.fit.meta['dependent']
-        print(y)
-        self.subplot.set_ylabel(
-            r'$\Delta R_{\text{NL}}$' + '$(\si{' + y['siunitx'] +  r'})$')
-
-    def add_text_table(self, rows, r0, dr, **kwargs):
-        for m, row in enumerate(rows):
-            for n, column in enumerate(row):
-                self.subplot.text(r0[0] + dr[0] * n, r0[1] + dr[1] * m, column,
-                    transform=self.subplot.axes.transAxes, **kwargs)
+        text = r'$\Delta R_{\text{NL}}$' + '$(\si{' + y['siunitx'] +  r'})$'
+        super().add_ylabel(text)
 
     def add_parameter_overlay(self):
         rows = self.parameter_table(self.fit.meta['fixed_parameters'][0])
-        self.subplot.text(0.5, 1.1, ' '.join(rows[0]),
+        self.plt.text(0.5, 1.1, ' '.join(rows[0]),
             horizontalalignment='center',
             verticalalignment='top',
-            transform=self.subplot.axes.transAxes)
+            transform=self.plt.axes.transAxes)
 
         rows = self.parameter_table(self.fit.meta['fitted_parameters'])
         self.add_text_table(rows[0:2], (0.05, 0.8), (0.07, -0.1),
@@ -39,20 +27,8 @@ class Plot(Plot):
             horizontalalignment='left',
             verticalalignment='top')
 
-    @staticmethod
-    def parameter_table(parameters):
-        if not isinstance(parameters, list): parameters = [parameters]
-        rows = []
-        for param in parameters:
-            row = []
-            row.append('$' + param['tex_symbol'] + '$')
-            row.append('$=$')
-            row.append(r'$\SI{' + param['disply_value'] + '}{' + param['siunitx'] + '}$')
-            rows.append(row)
-        return rows
-
 def main():
-    figure = matplotlib.pyplot.figure(figsize=(5,10))
+    figure = Plot.new_figure(figsize=(5,10))
 
     data_path = lambda x: os.path.join('data', 'PhysRevLett.105.167202',
         'fig_4' + x + '_difference.json')
@@ -66,7 +42,7 @@ def main():
         n, m = 3, 1
 
         options = {}
-        if idx != 0: options['sharex'] = plots[idx - 1].subplot
+        if idx != 0: options['sharex'] = plots[idx - 1].plt
 
         plot = Plot(fit, figure.add_subplot(n, m, (m * idx + 1), **options))
         plots.append(plot)
@@ -78,10 +54,10 @@ def main():
         plot.add_parameter_overlay()
 
     plots[-1].add_xlabel()
-    matplotlib.pyplot.setp([ p.subplot.get_xticklabels() for p in plots[0:2] ], visible=False)
+    matplotlib.pyplot.setp([ p.plt.get_xticklabels() for p in plots[0:2] ], visible=False)
 
     figure.savefig(os.path.join('build', 'plot_difference.eps'), transparent=True)
-    matplotlib.pyplot.close(figure)
+    Plot.close_figure(figure)
 
     info = [ '$' + v['tex_symbol'] + ' = \SI{' + v['disply_value'] + '}{' + v['siunitx'] + '}$'
         for v in plots[0].fit.meta['fixed_parameters']
