2ef65df7ea12bf7fa19a8c799b10ed10ed320f74
Evan Sosenko <razorx@evansosenko.com>
2014-03-24 13:14:47 -0700

    Moved add_parameter_overlay to Plot class.

--- plots/plot.py
+++ plots/plot.py
@@ -8,3 +8,24 @@ class Plot(Plot):
     @id.setter
     def id(self, value):
         self._id = value
+
+    def add_parameter_overlay(self):
+        rows = self.parameter_table(self.fit.meta['fixed_parameters'][0])
+
+        text = []
+        text.append('(' + self.id + ') ')
+        text.append(' '.join(rows[0]))
+        text.append(', ' + self.fit.meta['contact_type'] + ' contacts')
+        self.plt.text(0.5, 1.1, ''.join(text),
+            horizontalalignment='center',
+            verticalalignment='top',
+            transform=self.plt.axes.transAxes)
+
+        rows = self.parameter_table(self.fit.meta['fitted_parameters'])
+        self.add_text_table(rows[0:2], (0.05, 0.8), (0.07, -0.1),
+            horizontalalignment='left',
+            verticalalignment='top')
+
+        self.add_text_table(rows[2:4], (0.6, 0.8), (0.07, -0.1),
+            horizontalalignment='left',
+            verticalalignment='top')
--- plots/plot_difference.py
+++ plots/plot_difference.py
@@ -11,27 +11,6 @@ class Plot(Plot):
         text = r'$\Delta R_{\text{NL}}$' + '$(\si{' + y['siunitx'] +  r'})$'
         super().add_ylabel(text)
 
-    def add_parameter_overlay(self):
-        rows = self.parameter_table(self.fit.meta['fixed_parameters'][0])
-
-        text = []
-        text.append('(' + self.id + ') ')
-        text.append(' '.join(rows[0]))
-        text.append(', ' + self.fit.meta['contact_type'] + ' contacts')
-        self.plt.text(0.5, 1.1, ''.join(text),
-            horizontalalignment='center',
-            verticalalignment='top',
-            transform=self.plt.axes.transAxes)
-
-        rows = self.parameter_table(self.fit.meta['fitted_parameters'])
-        self.add_text_table(rows[0:2], (0.05, 0.8), (0.07, -0.1),
-            horizontalalignment='left',
-            verticalalignment='top')
-
-        self.add_text_table(rows[2:4], (0.6, 0.8), (0.07, -0.1),
-            horizontalalignment='left',
-            verticalalignment='top')
-
 def main():
     figure = Plot.new_figure(figsize=(5,10))
 
