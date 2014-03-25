ebdbd8dc3444f427480535400f48effe242093e1
Evan Sosenko <razorx@evansosenko.com>
2014-03-25 15:03:48 -0700

    Move json/PhysRevLett.105.167202 to json.

--- plots/plot_difference.py
+++ plots/plot_difference.py
@@ -14,8 +14,7 @@ class Plot(Plot):
 def main():
     figure = Plot.new_figure(figsize=(5,10))
 
-    data_path = lambda x: os.path.join('json', 'PhysRevLett.105.167202',
-        'fig_4' + x + '_difference.json')
+    data_path = lambda x: os.path.join('json', 'fig_4' + x + '_difference.json')
     fits = [ Fit(data_path(fig)) for fig in ['a', 'b', 'd'] ]
 
     for fit in fits[0:2]:
